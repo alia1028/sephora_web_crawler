aecdef4ee492e9c94051243bb083788a792cd5c6
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-04-29 12:25:13 -0400

    finish crawling

--- scrapeandseed.js
+++ scrapeandseed.js
@@ -46,19 +46,123 @@ async function pageTwo() {
   const { data } = await axios.get(
     "https://www.sephora.com/api/catalog/categories/cat1960033/products?currentPage=2&pageSize=300&content=true&includeRegionsMap=true"
   );
+  const products = response.data.products;
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const productInfo = [];
+  let count = 1;
+  products.forEach((product) => {
+    product.id = count;
+    productInfo.push(product);
+    count++;
+  });
+
+  productInfo.forEach(async (product, index) => {
+    try {
+      const page = await browser.newPage();
+      await page.setDefaultNavigationTimeout(0);
+      page.setUserAgent(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+      );
+      await page.goto(`https://www.sephora.com${product.targetUrl}`, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+      const userAgent = await page.evaluate(() => navigator.userAgent);
+      const productContent = await page.content();
+      const $ = await cheerio.load(productContent);
+      const ingredient = await $(
+        "#ingredients .css-1ue8dmw.eanm77i0 div"
+      ).text();
+      product.ingredients = ingredient;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  console.log(productInfo);
 }
 
 async function pageThree() {
   const { data } = await axios.get(
     "https://www.sephora.com/api/catalog/categories/cat1960033/products?currentPage=3&pageSize=300&content=true&includeRegionsMap=true"
   );
-  console.log(data.products);
+  const products = response.data.products;
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const productInfo = [];
+  let count = 1;
+  products.forEach((product) => {
+    product.id = count;
+    productInfo.push(product);
+    count++;
+  });
+
+  productInfo.forEach(async (product, index) => {
+    try {
+      const page = await browser.newPage();
+      await page.setDefaultNavigationTimeout(0);
+      page.setUserAgent(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+      );
+      await page.goto(`https://www.sephora.com${product.targetUrl}`, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+      const userAgent = await page.evaluate(() => navigator.userAgent);
+      const productContent = await page.content();
+      const $ = await cheerio.load(productContent);
+      const ingredient = await $(
+        "#ingredients .css-1ue8dmw.eanm77i0 div"
+      ).text();
+      product.ingredients = ingredient;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  console.log(productInfo);
 }
 
 async function pageFour() {
   const { data } = await axios.get(
     "https://www.sephora.com/api/catalog/categories/cat1960033/products?currentPage=4&pageSize=300&content=true&includeRegionsMap=true"
   );
+  const products = response.data.products;
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const productInfo = [];
+  let count = 1;
+  products.forEach((product) => {
+    product.id = count;
+    productInfo.push(product);
+    count++;
+  });
+
+  productInfo.forEach(async (product, index) => {
+    try {
+      const page = await browser.newPage();
+      await page.setDefaultNavigationTimeout(0);
+      page.setUserAgent(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+      );
+      await page.goto(`https://www.sephora.com${product.targetUrl}`, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+      const userAgent = await page.evaluate(() => navigator.userAgent);
+      const productContent = await page.content();
+      const $ = await cheerio.load(productContent);
+      const ingredient = await $(
+        "#ingredients .css-1ue8dmw.eanm77i0 div"
+      ).text();
+      product.ingredients = ingredient;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  console.log(productInfo);
 }
 
 pageOne();
+pageTwo();
+pageThree();
+pageFour();
